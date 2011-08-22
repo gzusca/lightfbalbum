b72f88c9d9b81d8fdd610703f60ff40b4ef926c2
Raymond <raymond@lmws.net>
2011-08-21 23:55:38 -0700

    feed json to galleria instead of dom

--- lightfbalbum.js
+++ lightfbalbum.js
@@ -5,18 +5,14 @@ $('.album').each(function(index, album) {
   var album_id = album.data('album-id');
   var url = album.data('use-cache') ? 'cache/' + album_id + '.json' : 'http://graph.facebook.com/' + album_id + '/photos?limit=100&callback=?';
   $.getJSON(url, function(response) {
+    var galleriaData = [];
     $(response.data).each(function(index, photo) {
-      var div = $('<div></div>');
-      div.attr('id', 'photo-' + album.id);
-      div.css('display', 'inline-block');
-      $(div).append('<a href="' + photo.source + '" target="_blank"><img src="' + photo.picture + '" /></a>');
-      $(album).append(div).append(' ');
+      galleriaData.push({
+        thumb: photo.picture,
+	image: photo.source,
+	link: photo.link
+      });
     });
+    $(album).galleria({ dataSource: galleriaData, width: 650, height: 500 });
   });
-});
-
-$(window).load(function() {
-  if (!Galleria) return;
-
-  $('.album').galleria({ width: 650, height: 500 });
 });
