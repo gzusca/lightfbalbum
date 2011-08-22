352a1fc31d985eca7ab6c2b9d4f5454a0bf52ace
Raymond <raymond@lmws.net>
2011-08-21 23:33:43 -0700

    added cache/facebook api split

--- lightfbalbum.js
+++ lightfbalbum.js
@@ -1,21 +1,22 @@
 Lightfbalbum = {};
-Lightfbalbum.setup = function(data) {
-  console.info(data);
-  return;
-  $('.album').each(function(index, album) {
-    album = $(album);
-    var album_id = album.data('album-id');
-    //$.get('tmp/' + album_id + '.json', function(response) {
-    $(data.data).each(function(index, photo) {
+
+$('.album').each(function(index, album) {
+  album = $(album);
+  var album_id = album.data('album-id');
+  var url = album.data('use-cache') ? 'cache/' + album_id + '.json' : 'http://graph.facebook.com/' + album_id + '/photos?limit=100&callback=?';
+  $.getJSON(url, function(response) {
+    $(response.data).each(function(index, photo) {
       var div = $('<div></div>');
       div.attr('id', 'photo-' + album.id);
       div.css('display', 'inline-block');
       $(div).append('<a href="' + photo.source + '" target="_blank"><img src="' + photo.picture + '" /></a>');
       $(album).append(div).append(' ');
     });
-
-    if (Galleria) {
-      $('.album').galleria({ width: 650, height: 500 });
-    }
   });
-};
+});
+
+$(window).load(function() {
+  if (!Galleria) return;
+
+  $('.album').galleria({ width: 650, height: 500 });
+});
